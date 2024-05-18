263cadabf503862dabcd8a98fa45646ce35b81c2
Animesh Maiti <a.maiti770@gmail.com>
2024-05-18 22:01:41 +0530

    connect mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const { db } = require('./db/db');
+const {readdirSync} = require('fs');
 
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
+// Routes
+readdirSync('./routes').map((r)=> app.use('/api', require(`./routes/${r}`)));
+
+const server =()=>{
+    db();
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+server();
