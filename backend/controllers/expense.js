d68a8033006547bcfa2cdba0faf616951c0a1862
Animesh Maiti <a.maiti770@gmail.com>
2024-05-19 22:07:15 +0530

    income expense req schema CRD

---
+++ backend/controllers/expense.js
@@ -0,0 +1,47 @@
+const ExpenseSchema = require("../models/expenseModel");
+
+
+// date format: mm/dd/yyyy
+exports.addExpense = async (req, res) => {
+    try {
+        const { title, amount, date, category, description } = req.body;
+
+        const expense = ExpenseSchema({
+            title,
+            amount,
+            date,
+            category,
+            description
+        });
+        if (!title || !date || !category || !description) {
+            return res.status(400).json({ msg: "All fields are required" });
+        }
+        if (amount <= 0 || !amount === Number) {
+            return res.status(400).json({ msg: "Amount should be a number and positive" });
+        }
+        await expense.save();
+        res.status(200).json(expense);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+    // res.json(req.body);
+}
+
+exports.getExpenses = async (req, res) => {
+    try {
+        const expenses = await ExpenseSchema.find().sort({ createdAt: -1 });
+        res.status(200).json(expenses);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+exports.deleteExpense = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await ExpenseSchema.findByIdAndDelete(id);
+        res.status(200).json({ msg: `id:${id} expense deleted` });
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
---
+++ backend/controllers/income.js
@@ -0,0 +1,46 @@
+const IncomeSchema = require("../models/incomeModel");
+
+// date format: mm/dd/yyyy
+exports.addIncome = async (req, res) => {
+    try {
+        const { title, amount, date, category, description } = req.body;
+
+        const income = IncomeSchema({
+            title,
+            amount,
+            date,
+            category,
+            description
+        });
+        if (!title || !date || !category || !description) {
+            return res.status(400).json({ msg: "All fields are required" });
+        }
+        if (amount <= 0 || !amount === Number) {
+            return res.status(400).json({ msg: "Amount should be a number and positive" });
+        }
+        await income.save();
+        res.status(200).json(income);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+    // res.json(req.body);
+}
+
+exports.getIncomes = async (req, res) => {
+    try {
+        const incomes = await IncomeSchema.find().sort({ createdAt: -1 });
+        res.status(200).json(incomes);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+exports.deleteIncome = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await IncomeSchema.findByIdAndDelete(id);
+        res.status(200).json({ msg: `id:${id} Income deleted` });
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
