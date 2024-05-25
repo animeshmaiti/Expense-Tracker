dec5276008f1b13458e7adc544abb92413b3e2bc
Animesh Maiti <a.maiti770@gmail.com>
2024-05-25 21:38:17 +0530

    fix some issues and delete-income

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -70,7 +70,7 @@ export const Form = () => {
             </option>
             <option value="salary">Salary</option>
             <option value="freelancing">Freelancing</option>
-            <option value="investments">Investiments</option>
+            <option value="investments">Investments</option>
             <option value="stocks">Stocks</option>
             <option value="bitcoin">Bitcoin</option>
             <option value="bank">Bank Transfer</option>
--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -1,34 +1,55 @@
-import { createContext,useState,useContext} from "react";
+import { createContext, useState, useContext,useCallback } from "react";
 import axios from "axios";
 
 const BaseUrl = 'http://localhost:5000/api/';
 
 const GlobalContext = createContext();
 
-export const GlobalProvider = ({children})=>{
+export const GlobalProvider = ({ children }) => {
 
     const [incomes, setIncomes] = useState([]);
     const [expenses, setExpenses] = useState([]);
     const [error, setError] = useState(null);
 
-    const addIncome = async (income)=>{
+    const getIncomes = useCallback(async () => {
         try {
-            const response = await axios.post(`${BaseUrl}add-income`,income);
-            // setIncomes([...incomes,data]);
+            const response = await axios.get(`${BaseUrl}get-incomes`);
+            setIncomes(response.data);
         } catch (err) {
-            setError(err.response.data.message);
+            setError(err.response?.data?.message);
         }
-    }
+    }, []);
+
+    const addIncome = useCallback(async (income) => {
+        try {
+            await axios.post(`${BaseUrl}add-income`, income);
+            getIncomes(); // Fetch the latest incomes after adding a new one
+        } catch (err) {
+            setError(err.response?.data?.message);
+        }
+    }, [getIncomes]);
+
+    const deleteIncome = useCallback(async (id) => {
+        try {
+            await axios.delete(`${BaseUrl}delete-income/${id}`);
+            getIncomes(); // Fetch the latest incomes after deleting one
+        } catch (err) {
+            setError(err.response?.data?.message);
+        }
+    }, [getIncomes]);
 
     return (
         <GlobalContext.Provider value={{
-            addIncome
+            addIncome,
+            getIncomes,
+            incomes,
+            deleteIncome
         }}>
             {children}
         </GlobalContext.Provider>
     )
 };
 
-export const useGlobalContext = ()=>{
+export const useGlobalContext = () => {
     return useContext(GlobalContext);
 }
